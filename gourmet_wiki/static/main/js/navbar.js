24e3ecbfbd5acc7fa849f489a2d64de11af70a75
schuggermi <mschuchowski@gmx.de>
2023-01-08 13:25:58 +0100

    changed settings static routes

---
+++ gourmet_wiki/static/main/js/navbar.js
@@ -0,0 +1,36 @@
+let navbarToggler = document.querySelector('#navbar-toggler')
+let navbarDropdown = document.querySelector('#navbar-dropdown')
+
+const togglerJumpingIn = [
+    { transform: 'translateY(-3px)' },
+]
+
+const togglerJumpingOut = [
+    { transform: 'translateY(3px)' },
+]
+
+const togglerTiming = {
+    duration: 300,
+    fill: 'forwards',
+}
+
+const togglerRotate = [
+    { transform: `rotate(5deg)` },
+]
+
+navbarDropdown.style.offsetHeight = 0
+
+navbarToggler.addEventListener("mouseover", (event) => {
+    event.preventDefault()
+    event.target.animate(togglerJumpingIn, togglerTiming)
+})
+
+navbarToggler.addEventListener("mouseout", (event) => {
+    event.preventDefault()
+    event.target.animate(togglerJumpingOut, togglerTiming)
+})
+
+navbarToggler.addEventListener("click", (event) => {
+    event.preventDefault()
+    navbarDropdown.classList.toggle('navbar-dropdown--slidein')
+})

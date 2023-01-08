3e7f6a51f2faa37aab1a7c1ca18261687d081251
schuggermi <mschuchowski@gmx.de>
2023-01-08 13:19:25 +0100

    change gitignore to add staticfiles and change settings

--- gourmet_wiki/gourmet_wiki/settings.py
+++ gourmet_wiki/gourmet_wiki/settings.py
@@ -109,13 +109,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+# https://docs.djangoproject.com/en/4.1/ref/settings/#defaulct-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
